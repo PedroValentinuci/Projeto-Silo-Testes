a750a63901ea369aedaac4435edaa9b4cff05c86
Pedro Valentinuci <30358845+PedroValentinuci@users.noreply.github.com>
2024-10-16 10:34:43 -0300

    dsdas

--- Projeto Silo/src/main.cpp
+++ Projeto Silo/src/main.cpp
@@ -1,10 +1,36 @@
 #include <Arduino.h>
+ 
+#define LED_VERDE 2
+#define LED_AMARELO 4
+#define LED_VERMELHO 5
 
+const int potPin = 34;
+int potValue = 0;
+ 
 void setup() {
-  Serial.begin(115200);
+  pinMode(LED_VERDE, OUTPUT);
+  pinMode(LED_AMARELO, OUTPUT);
+  pinMode(LED_VERMELHO, OUTPUT);
+  Serial.begin(9600);
 }
-
+ 
 void loop() {
-  Serial.println("Hello, world!");
-  delay(1000);
+  potValue = analogRead(potPin);
+  Serial.println(potValue);
+  delay(500);
+  if (potValue >= 0 && potValue <= 1000)
+  {
+    digitalWrite(LED_VERDE, HIGH);
+    Serial.println("Led Verde acesso!");
+  }
+  else if (potValue >= 1001 && potValue <= 2000)
+  {
+    digitalWrite(LED_AMARELO, HIGH);
+    Serial.println("Led Amarelo acesso!");
+  }
+  else 
+  {
+    digitalWrite(LED_VERMELHO, HIGH);
+    Serial.println("Led Vermelho acesso!");
+  }
 }
